7949394b034235c0cddeaa6eae7f0a4c0e88a497
Jude87 <107044598+Jude87@users.noreply.github.com>
2022-07-07 17:39:03 +0900

    Add files via upload

---
+++ RUN.java
@@ -0,0 +1,12 @@
+package practice.operator;
+
+public class RUN {
+
+	public static void main(String[] args) {
+		OperatorPractice op = new OperatorPractice();
+		//op.method1();
+		op.method2();
+
+	}
+
+}
